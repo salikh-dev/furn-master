41b18927c3437a944480e5aef7efacfec257d312
Bekzodbek <97502548+Bekzodbek2006@users.noreply.github.com>
2022-08-03 13:14:20 +0500

    commit

--- furn/form.py
+++ furn/form.py
@@ -1,12 +1,3 @@
 from django import forms
-from dataclasses import fields
-from .models import Subscribe
 
-User = Subscribe
 
-class Followers(forms.ModelForm):
-    class Meta:
-        model = User
-        fields = (
-            'email',
-        )
