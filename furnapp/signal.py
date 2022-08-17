f3a7ddad701bb41d4cbd48e367f92dfcaceced74
Bekzodbek <97502548+Bekzodbek2006@users.noreply.github.com>
2022-08-17 14:10:00 +0500

    commit

--- furnapp/signal.py
+++ furnapp/signal.py
@@ -1,4 +1,17 @@
+import profile
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from .models import MyUser, Profile
+from .models import Profile
+from django.contrib.auth import get_user_model
 
+User = get_user_model()
+
+@receiver(post_save, sender=MyUser)
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(MyUser=instance)
+
+@receiver(post_save, sender=User)
+
+def save_profile(sender, instanse, **kwargs):
+    instanse.profile.save()
